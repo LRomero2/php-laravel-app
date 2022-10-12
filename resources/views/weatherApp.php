e1d194026e9cc5a8bf4183495a74bdda54e7824d
Romero-Lauren <lauren.romero@uniqueiq.co.uk>
2022-10-12 10:26:02 +0100

    Started up project and created new files for front-end and set up api for weather.

---
+++ resources/views/weatherApp.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Weather App</title>
+  </head>
+  <body>
+      <div class="search-container">
+    <h1>Search Weather</h1>
+    <form action="" method="GET">
+        <p><label for="city">Enter your City Name</label></p>
+       <p><input type="text" name="city" id="city" placeholder="City Name"></p>
+        <button type="submit" name="submit" class="btn btn-success">Submit</button>
+    
+        <div class="output">
+
+
+</div>
+    </form>
+</div>
+
+
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
